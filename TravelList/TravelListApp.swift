6b21ae2537120251bb90bd13ff4a66952e2a6d11
edhoguntur <edho.guntur@gmail.com>
2023-11-02 14:10:27 +0700

    Initial Commit

---
+++ TravelList/TravelListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelListApp.swift
+//  TravelList
+//
+//  Created by Edho Guntur Adhitama on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
