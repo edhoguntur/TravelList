3dae90bd0e3a69bf37d04e4c507cc630be128d58
edhoguntur <edho.guntur@gmail.com>
2023-11-03 19:25:22 +0700

    Add relationship for model sight

---
+++ TravelList/Model/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  TravelList
+//
+//  Created by Edho Guntur Adhitama on 03/11/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
